b32e89d8d3391fc5cfaa31981a0f898ecd10f746
Young Ji Kim <youjmik@gmail.com>
2021-03-15 16:17:22 -0400

    adding additional folders and files

---
+++ models/workout.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose")
+
+const Schema = mongoose.Schema
+
+const workoutSchema = new Schema ({
+    date: {
+        type: Date,
+        default: Date.now
+    },
+    exercise: [{
+        type: {
+            type: String, 
+            enum:['Cardio','Resistance'],
+            trim: true,
+        },
+        name: {
+            type: String,
+            trim: true,
+        },
+        weight: {
+            type: Number,
+        },
+        sets: {
+            type: Number,
+        },
+        reps: {
+            type:Number,
+        },
+        duration: {
+            type: Number,
+        },
+
+    }]
+});
+
+const Workout = mongoose.model("Workout", workoutSchema)
+module.exports = Workout;
---
+++ routes/api-routes.js
@@ -0,0 +1,31 @@
+const Workout = require("../models/workout");
+
+module.exports =(app) => {
+
+    app.get("/api/workouts", (req,res) =>{
+        Workout.find({})
+        .then(dbWorkout => {
+            res.json(dbWorkout)
+        })
+        .catch(err => {
+            res.status(400).json(err)
+        })
+    });
+
+    app.post("/api/workouts",(req,res)=>{
+        Workout.create({exercise: [req.body]})
+        .then(dbWorkout => {
+            res.json(dbWorkout)
+        })
+        .catch(err => {
+            res.status(400).json(err)
+        })
+    });
+    
+    app.put("/api/workouts/:id",(req,res)=>{
+        Workout.findOneAndUpdate({_id:req.params.id}, {$push: {exercise:req.body}},{new:true})
+    })
+
+}
+
+
